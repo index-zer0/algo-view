cd7cad587bda52ab3d426b786433258a2d1ff413
NefeliTav <msstama2@yahoo.gr>
2021-02-16 19:17:55 +0200

    add cards in home page

--- src/App.js
+++ src/App.js
@@ -10,35 +10,19 @@ const App = () => {
 				<a href="/algo-view" className="logo">
 					Algo view
 				</a>
-				<div className="navbar">
-					<div className="subnav">
-						<button className="subnavbtn">About <i className="fa fa-caret-down"></i></button>
-						<div className="subnav-content">
-							<a href="#company">Company</a>
-							<a href="#team">Team</a>
-							<a href="#careers">Careers</a>
-						</div>
-					</div>
-					<div className="subnav">
-						<button className="subnavbtn">Services <i className="fa fa-caret-down"></i></button>
-						<div className="subnav-content">
-							<a href="#bring">Bring</a>
-							<a href="#deliver">Deliver</a>
-							<a href="#package">Package</a>
-							<a href="#express">Express</a>
-						</div>
-					</div>
-					<div className="subnav">
-						<button className="subnavbtn">Contact us<i className="fa fa-caret-down"></i></button>
-					</div>
+				<div className="topnav" id="myTopnav">
+					<a href="#home" className="active">Home</a>
+					<a href="#news">News</a>
+					<a href="#contact">Contact</a>
+					<a href="#about">About</a>
 				</div>
 
 				<div>
-					<input type="checkbox" class="checkbox" id="chk" />
-					<label class="label" for="chk">
-						<i class="fas fa-moon"></i>
-						<i class="fas fa-sun"></i>
-						<div class="ball"></div>
+					<input type="checkbox" className="checkbox" id="chk" />
+					<label className="label" for="chk">
+						<i className="fas fa-moon"></i>
+						<i className="fas fa-sun"></i>
+						<div className="ball"></div>
 					</label>
 				</div>
 
@@ -52,7 +36,55 @@ const App = () => {
 				<Router basename={process.env.PUBLIC_URL}>
 					<Switch>
 						<Route exact path="/">
-							<div className="container">Home</div>
+							<div className="card" style={{ marginTop: "5rem" }}>
+
+								<div className="header">
+									<p>Sorting Algorithms</p>
+								</div>
+								<div className="container">
+									<ul>
+										<li><a className="url" href="url">Bubble Sort</a></li>
+										<li><a className="url" href="url">Insertion Sort</a></li>
+										<li><a className="url" href="url">Selection Sort</a></li>
+										<li><a className="url" href="url">Quick Sort</a></li>
+									</ul>
+									<br></br>
+									<button class="button" >Go</button>
+								</div>
+							</div>
+							<div className="card" style={{ marginLeft: "1rem", marginTop: "5rem" }}>
+
+								<div className="header">
+									<p>Search Algorithms</p>
+								</div>
+								<div className="container">
+									<ul>
+										<li><a className="url" href="url">Depth First Search</a></li>
+										<li><a className="url" href="url">Breadth First Search</a></li>
+										<li><a className="url" href="url">A *</a></li>
+										<li><a className="url" href="url">Djikstra's</a></li>
+									</ul>
+									<br></br>
+									<button class="button" >Go</button>
+								</div>
+							</div>
+							<div className="card" style={{ marginLeft: "1rem", marginTop: "5rem" }}>
+
+								<div className="header">
+									<p>Calculatos</p>
+								</div>
+								<div className="container">
+									<ul>
+										<li><a className="url" href="url">Calculus</a></li>
+										<li><a className="url" href="url">Calculus II</a></li>
+										<li><a className="url" href="url">Linear Algebra</a></li>
+										<li><a className="url" href="url">Discrete Mathematics</a></li>
+									</ul>
+									<br></br>
+									<button class="button" >Go</button>
+								</div>
+							</div>
+
 						</Route>
 						<Route path="/sort" component={SortAlgorithms} />
 					</Switch>
@@ -62,12 +94,20 @@ const App = () => {
 		</div>
 	);
 };
+function myFunction() {
+	var x = document.getElementById("myTopnav");
+	if (x.className === "topnav") {
+		x.className += " responsive";
+	} else {
+		x.className = "topnav";
+	}
+}
 /*
 const chk = document.getElementById('chk');
 if (chk) {
-	chk.addEventListener('change', () => {
-		document.body.classList.toggle('dark');
-	});
+				chk.addEventListener('change', () => {
+					document.body.classList.toggle('dark');
+				});
 
 }
 */
